0a21446754627b323e2520a069184a24b3766718
Ryan Workman <rdavid1099@gmail.com>
2017-01-23 16:29:22 -0700

    Fix Twitter Omniauth and issue with env variables

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :twitter, ENV['TWITTER-API-KEY'], ENV['TWITTER-API-SECRET'],
+    {
+      secure_image_url: 'true',
+      image_size: 'original',
+      authorize_params: {
+        force_login: 'true'
+      }
+    }
 end
